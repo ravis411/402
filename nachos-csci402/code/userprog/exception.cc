31645d7e9425055cb5985bd8da84d6ade9e472d5
ryanwdav <ryanwdav@usc.edu>
2015-10-25 15:32:31 -0700

    no divRoundup

--- nachos-csci402/code/userprog/exception.cc
+++ nachos-csci402/code/userprog/exception.cc
@@ -721,7 +721,7 @@ int currentTLB = 0; //To keep track of which TLB entry to replace
 //Handles a TLB miss / PageFaultException
 void handleTLBMiss(){
 	unsigned int VA = machine->ReadRegister(BadVAddrReg);
-	unsigned int VP = divRoundUp(VA, PageSize);
+	unsigned int VP = VA / PageSize;
 	DEBUG('T', "Need virtual address %i in virtual page %i\n", VA, VP);
 
 	IntStatus oldLevel = interrupt->SetLevel(IntOff);   // disable interrupts
