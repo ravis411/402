9a87922eda51eaa5588f62437225c52228e77439
ryanwdav <ryanwdav@usc.edu>
2015-11-07 20:45:09 -0800

    There is now a test suite.

--- nachos-csci402/code/test/proj3TestSuite.c
+++ nachos-csci402/code/test/proj3TestSuite.c
@@ -6,9 +6,21 @@
 
 #include "syscall.h"
 
+char welcomeString[] = "\nproj3TestSuite.\n\n";
+char matFileName[] = "testVirtualMemory.c";
+char sortFileName[] = "sort.c";
 
 int
 main()
 {
-    PrintString("\n\nTest Concluded Succesfully.\n\n", sizeof("\n\nTest Concluded Succesfully.\n\n"));
+    PrintString(welcomeString, sizeof(welcomeString));
+
+    PrintString("Going to Exec 2 testVirtualMemory.c and 1 sort.c\n", sizeof("Going to Exec 2 testVirtualMemory.c and 1 sort.c\n"));
+    PrintString("\ttestVirtualMemory forks 2 matmults.\n", sizeof("\ttestVirtualMemory forks 2 matmults.\n"));
+    PrintString("\tExpected output is 5 Exit statuses, 4 7220s and 1 1023\n\n", sizeof("\tExpected output is 5 Exit statuses, 4 7220s and 1 1023\n\n"));
+
+	Exec(matFileName, sizeof(matFileName));
+	Exec(matFileName, sizeof(matFileName));
+	Exec(sortFileName, sizeof(sortFileName));
+
 }
---
+++ nachos-csci402/code/test/testVirtualMemory.c
@@ -0,0 +1,70 @@
+/* matmult.c 
+ *    Test program to do matrix multiplication on large arrays.
+ *
+ *    Intended to stress virtual memory system.
+ *
+ *    Ideally, we could read the matrices off of the file system,
+ *	and store the result back to the file system!
+ */
+
+#include "syscall.h"
+
+#define Dim 	20	/* sum total of the arrays doesn't fit in 
+			 * physical memory 
+			 */
+
+int A[Dim][Dim];
+int B[Dim][Dim];
+int C[Dim][Dim];
+
+int D[Dim][Dim];
+int E[Dim][Dim];
+int F[Dim][Dim];
+
+void mat1(){
+	 int i, j, k;
+
+    for (i = 0; i < Dim; i++)		/* first initialize the matrices */
+	for (j = 0; j < Dim; j++) {
+	     A[i][j] = i;
+	     B[i][j] = j;
+	     C[i][j] = 0;
+	}
+
+    for (i = 0; i < Dim; i++)		/* then multiply them together */
+	for (j = 0; j < Dim; j++)
+            for (k = 0; k < Dim; k++)
+		 C[i][j] += A[i][k] * B[k][j];
+
+    Exit(C[Dim-1][Dim-1]);		/* and then we're done */
+}
+
+void mat2(){
+ int i, j, k;
+
+    for (i = 0; i < Dim; i++)		/* first initialize the matrices */
+	for (j = 0; j < Dim; j++) {
+	     D[i][j] = i;
+	     E[i][j] = j;
+	     F[i][j] = 0;
+	}
+
+    for (i = 0; i < Dim; i++)		/* then multiply them together */
+	for (j = 0; j < Dim; j++)
+            for (k = 0; k < Dim; k++)
+		 F[i][j] += D[i][k] * E[k][j];
+
+    Exit(F[Dim-1][Dim-1]);		/* and then we're done */
+}
+
+
+
+
+int
+main()
+{
+   PrintString("testVirtualMemory: Forking two matmults.\n", sizeof("testVirtualMemory: Forking two matmults.\n"));
+
+   Fork(mat1);
+   Fork(mat2);
+}
