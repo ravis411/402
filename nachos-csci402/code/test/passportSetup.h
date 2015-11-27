9ebaa7955d06a7ef9581bf6b9d326193e4313361
ryanwdav <ryanwdav@usc.edu>
2015-11-26 23:53:26 -0800

    there we go hopefully...

--- nachos-csci402/code/test/passportSetup.h
+++ nachos-csci402/code/test/passportSetup.h
@@ -283,9 +283,10 @@ void passportSetup(){
 
 	THEEND = CreateMV("THEEND", sizeof("THEEND"), 1);
 
+
   /*Init clerkStates, lineCounts*/
 	PrintString("Setup bout to create MAXCLERKS loop.\n", sizeof("Setup bout to create MAXCLERKS loop.\n"));
-  for(i=0; i<MAXCLERKS; i++){
+ /* for(i=0; i<MAXCLERKS; i++){
 
   	temp = "appLock";
   	StringConcatInt(temp, sizeof(temp), i);
@@ -309,7 +310,111 @@ void passportSetup(){
     cashierLineCV[i] = CreateCondition("casLineCV" + i, sizeof("casLineCV" + i));
     cashierBribeLineCV[i] = CreateCondition("casBribeCV" + i, sizeof("casBribeCV" + i));
     cashierCV[i] = CreateCondition("casCV" + i, sizeof("casCV" + i));
-  }
+  }*/
+
+   	applicationClerkLock[0] = CreateLock("appLock1", sizeof("appLock1"));
+    applicationClerkLineCV[0] = CreateCondition("appLineCV1", sizeof("appLineCV1"));
+    applicationClerkBribeLineCV[0] = CreateCondition("appBribeCV1", sizeof("appBribeCV1"));
+    applicationClerkCV[0] = CreateCondition("appCV1", sizeof("appCV1"));
+
+    applicationClerkLock[1] = CreateLock("appLock2", sizeof("appLock2"));
+    applicationClerkLineCV[1] = CreateCondition("appLineCV2", sizeof("appLineCV2"));
+    applicationClerkBribeLineCV[1] = CreateCondition("appBribeCV2", sizeof("appBribeCV2"));
+    applicationClerkCV[1] = CreateCondition("appCV2", sizeof("appCV2"));
+
+    applicationClerkLock[2] = CreateLock("appLock3", sizeof("appLock3"));
+    applicationClerkLineCV[2] = CreateCondition("appLineCV3", sizeof("appLineCV3"));
+    applicationClerkBribeLineCV[2] = CreateCondition("appBribeCV3", sizeof("appBribeCV3"));
+    applicationClerkCV[2] = CreateCondition("appCV3", sizeof("appCV3"));
+
+    applicationClerkLock[3] = CreateLock("appLock4", sizeof("appLock4"));
+    applicationClerkLineCV[3] = CreateCondition("appLineCV4", sizeof("appLineCV4"));
+    applicationClerkBribeLineCV[3] = CreateCondition("appBribeCV4", sizeof("appBribeCV4"));
+    applicationClerkCV[3] = CreateCondition("appCV4", sizeof("appCV4"));
+
+    applicationClerkLock[4] = CreateLock("appLock5", sizeof("appLock5"));
+    applicationClerkLineCV[4] = CreateCondition("appLineCV5", sizeof("appLineCV5"));
+    applicationClerkBribeLineCV[4] = CreateCondition("appBribeCV5", sizeof("appBribeCV5"));
+    applicationClerkCV[4] = CreateCondition("appCV5", sizeof("appCV5"));
+
+
+    pictureClerkLock[0] = CreateLock("picLock1", sizeof("picLock1"));
+    pictureClerkLineCV[0] = CreateCondition("picLineCV1", sizeof("picLineCV1"));
+    pictureClerkBribeLineCV[0] = CreateCondition("picBribeCV1", sizeof("picBribeCV1"));
+    pictureClerkCV[0] = CreateCondition("picCV1", sizeof("picCV1"));
+
+    pictureClerkLock[1] = CreateLock("picLock2", sizeof("picLock2"));
+    pictureClerkLineCV[1] = CreateCondition("picLineCV2", sizeof("picLineCV2"));
+    pictureClerkBribeLineCV[1] = CreateCondition("picBribeCV2", sizeof("picBribeCV2"));
+    pictureClerkCV[1] = CreateCondition("picCV2", sizeof("picCV2"));
+
+    pictureClerkLock[2] = CreateLock("picLock3", sizeof("picLock3"));
+    pictureClerkLineCV[2] = CreateCondition("picLineCV3", sizeof("picLineCV3"));
+    pictureClerkBribeLineCV[2] = CreateCondition("picBribeCV3", sizeof("picBribeCV3"));
+    pictureClerkCV[2] = CreateCondition("picCV3", sizeof("picCV3"));
+
+    pictureClerkLock[3] = CreateLock("picLock4", sizeof("picLock4"));
+    pictureClerkLineCV[3] = CreateCondition("picLineCV4", sizeof("picLineCV4"));
+    pictureClerkBribeLineCV[3] = CreateCondition("picBribeCV4", sizeof("picBribeCV4"));
+    pictureClerkCV[3] = CreateCondition("picCV4", sizeof("picCV4"));
+
+    pictureClerkLock[4] = CreateLock("picLock5", sizeof("picLock5"));
+    pictureClerkLineCV[4] = CreateCondition("picLineCV5", sizeof("picLineCV5"));
+    pictureClerkBribeLineCV[4] = CreateCondition("picBribeCV5", sizeof("picBribeCV5"));
+    pictureClerkCV[4] = CreateCondition("picCV5", sizeof("picCV5"));
+
+
+    passportClerkLock[0] = CreateLock("pasLock1", sizeof("pasLock1"));
+    passportClerkLineCV[0] = CreateCondition("pasLineCV1", sizeof("pasLineCV1"));
+    passportClerkBribeLineCV[0] = CreateCondition("pasBribeCV1", sizeof("pasBribeCV1"));
+    passportClerkCV[0] = CreateCondition("pasCV1", sizeof("pasCV1"));
+
+    passportClerkLock[1] = CreateLock("pasLock2", sizeof("pasLock2"));
+    passportClerkLineCV[1] = CreateCondition("pasLineCV2", sizeof("pasLineCV2"));
+    passportClerkBribeLineCV[1] = CreateCondition("pasBribeCV2", sizeof("pasBribeCV2"));
+    passportClerkCV[1] = CreateCondition("pasCV2", sizeof("pasCV2"));
+
+    passportClerkLock[2] = CreateLock("pasLock3", sizeof("pasLock3"));
+    passportClerkLineCV[2] = CreateCondition("pasLineCV3", sizeof("pasLineCV3"));
+    passportClerkBribeLineCV[2] = CreateCondition("pasBribeCV3", sizeof("pasBribeCV3"));
+    passportClerkCV[2] = CreateCondition("pasCV3", sizeof("pasCV3"));
+
+    passportClerkLock[3] = CreateLock("pasLock4", sizeof("pasLock4"));
+    passportClerkLineCV[3] = CreateCondition("pasLineCV4", sizeof("pasLineCV4"));
+    passportClerkBribeLineCV[3] = CreateCondition("pasBribeCV4", sizeof("pasBribeCV4"));
+    passportClerkCV[3] = CreateCondition("pasCV4", sizeof("pasCV4"));
+
+    passportClerkLock[4] = CreateLock("pasLock5", sizeof("pasLock5"));
+    passportClerkLineCV[4] = CreateCondition("pasLineCV5", sizeof("pasLineCV5"));
+    passportClerkBribeLineCV[4] = CreateCondition("pasBribeCV5", sizeof("pasBribeCV5"));
+    passportClerkCV[4] = CreateCondition("pasCV5", sizeof("pasCV5"));
+
+
+    cashierLock[0] = CreateLock("casLock1", sizeof("casLock1"));
+    cashierLineCV[0] = CreateCondition("casLineCV1", sizeof("casLineCV1"));
+    cashierBribeLineCV[0] = CreateCondition("casBribeCV1", sizeof("casBribeCV1"));
+    cashierCV[0] = CreateCondition("casCV1", sizeof("casCV1"));
+
+    cashierLock[1] = CreateLock("casLock2", sizeof("casLock2"));
+    cashierLineCV[1] = CreateCondition("casLineCV2", sizeof("casLineCV2"));
+    cashierBribeLineCV[1] = CreateCondition("casBribeCV2", sizeof("casBribeCV2"));
+    cashierCV[1] = CreateCondition("casCV2", sizeof("casCV2"));
+
+    cashierLock[2] = CreateLock("casLock3", sizeof("casLock3"));
+    cashierLineCV[2] = CreateCondition("casLineCV3", sizeof("casLineCV3"));
+    cashierBribeLineCV[2] = CreateCondition("casBribeCV3", sizeof("casBribeCV3"));
+    cashierCV[2] = CreateCondition("casCV3", sizeof("casCV3"));
+
+    cashierLock[3] = CreateLock("casLock4", sizeof("casLock4"));
+    cashierLineCV[3] = CreateCondition("casLineCV4", sizeof("casLineCV4"));
+    cashierBribeLineCV[3] = CreateCondition("casBribeCV4", sizeof("casBribeCV4"));
+    cashierCV[3] = CreateCondition("casCV4", sizeof("casCV4"));
+
+    cashierLock[4] = CreateLock("casLock5", sizeof("casLock5"));
+    cashierLineCV[4] = CreateCondition("casLineCV5", sizeof("casLineCV5"));
+    cashierBribeLineCV[4] = CreateCondition("casBribeCV5", sizeof("casBribeCV5"));
+    cashierCV[4] = CreateCondition("casCV5", sizeof("casCV5"));
+
   PrintString("Setup done...\n", sizeof("Setup done...\n"));
 }
 
@@ -480,13 +585,13 @@ void initialPassportSetup(){
 
 
   for(i = 0; i < CLERKCOUNT; i++){
-    /*Exec("../test/PAppClerk", sizeof("../test/PAppClerk"));*/
+    Exec("../test/PAppClerk", sizeof("../test/PAppClerk"));
     /*Exec("../test/PPicClerk", sizeof("../test/PPicClerk"));
     Exec("../test/PPasClerk", sizeof("../test/PPasClerk"));
     Exec("../test/PCashier", sizeof("../test/PCashier"));*/
   }
 
-  /*Exec("../test/PManager", sizeof("../test/PManager"));*/
+  Exec("../test/PManager", sizeof("../test/PManager"));
 
 }
 
