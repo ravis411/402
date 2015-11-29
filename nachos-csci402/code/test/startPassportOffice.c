e9e57902b3812fd731bb193d83d713456f138de8
ryanwdav <ryanwdav@usc.edu>
2015-11-29 13:04:46 -0800

    test

--- nachos-csci402/code/test/startPassportOffice.c
+++ nachos-csci402/code/test/startPassportOffice.c
@@ -14,12 +14,16 @@
 int
 main()
 {
-	char *input = "  ";
+	char input[15];
 	startPass();
 
+
+	PrintString("How many Customers: ", sizeof("How many Customers: "));
 	Read(input, sizeof(input), ConsoleInput);
 
+	PrintString("Input: ", sizeof("Input: "));
 	PrintString(input, sizeof(input));
+	PrintString("\n", 1);
 
 
 	
